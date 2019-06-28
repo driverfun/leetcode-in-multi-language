4499c07ca0817f41a974c9038035d8c28153fc7a
Kippa <donald86210@yahoo.com>
2019-06-28 18:05:03 +0800

    feat(two_sum): two_sum implement and test

---
+++ golang/1.two_sum/two_sum.go
@@ -0,0 +1,27 @@
+package two_sum
+
+// use map. O(n)
+func TwoSumV1(nums []int, target int) []int {
+	numMap := make(map[int]int, len(nums))
+	for i, v := range nums {
+		if j, ok := numMap[target-v]; ok {
+			return []int{j, i}
+		} else {
+			numMap[v] = i
+		}
+	}
+	return []int{-1, -2}
+}
+
+// scan in circle violently
+func TwoSumV2(nums []int, target int) []int {
+	n := len(nums)
+	for i := 0; i < n; i++ {
+		for j := i; j < n; j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{-1, -2}
+}
