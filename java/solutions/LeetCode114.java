fe44e6c31db2554ba2df76aa30fcd1e83163c83e
L1nf3ng <nuaa_llf@126.com>
2020-12-17 23:56:06 +0800

    add one more method and explanation for LeetCode_144.

--- java/solutions/LeetCode114.java
+++ java/solutions/LeetCode114.java
@@ -13,20 +13,24 @@ public class LeetCode114 implements SolutionsFacade {
     // 而链表顺序正好是前序遍历
     // 因此，方法一：利用额外的空间O(n)，n为二叉树节点个数
     //             按前序将节点填入列表，再依次重组他们的关系
-
-    public void preorder(List<TreeNode>list, TreeNode root){
+    //
+    //      方法二：这题目与昨天做的98题解法相同，都要记录其上一节点（注意设置为全局变量）
+    //             不同之处在于，在进行前序遍历时，需要保存上一节点的右子节点（否则返回后找不到右子树，注意：这里用局部变量，防止覆盖）
+    //              而左子树因为在遍历时必定是根节点的下一节点，所以可不保留。
+    //              如此，在每次进入一个节点后先将last节点左子节点置空，右子节点指向当前节点，再更新last为当前节点，并保存其右子树
+    public void preorder0(List<TreeNode>list, TreeNode root){
         if(root == null)
             return;
 
         list.add(root);
-        preorder(list, root.left);
-        preorder(list, root.right);
+        preorder0(list, root.left);
+        preorder0(list, root.right);
     }
 
 
     public void flatten0(TreeNode root) {
         List<TreeNode> nodeList = new ArrayList<>();
-        preorder(nodeList, root);
+        preorder0(nodeList, root);
         if (nodeList.size() > 0) {
             for (int j = 0; j < nodeList.size() - 1; j++) {
                 nodeList.get(j).left = null;
@@ -35,9 +39,9 @@ public class LeetCode114 implements SolutionsFacade {
             // 最后一个节点就免了，因为是叶子节点，所以左右节点比为空
         }
     }
+    public TreeNode last;
 
-
-    public void inorder(TreeNode origin, TreeNode last){
+    public void inorder(TreeNode origin){
         if(last == null)
             last = origin;
         else{
@@ -45,15 +49,17 @@ public class LeetCode114 implements SolutionsFacade {
             last.left = null;
         }
         TreeNode right = origin.right;
+        last = origin;
         if(origin.left!= null)
-            inorder(origin.left, origin);
+            inorder(origin.left);
         if(right != null)
-            inorder(right, origin);
+            inorder(right);
     }
 
     public void flatten(TreeNode root){
-
-
+        if(root==null)
+            return ;
+        inorder(root);
     }
 
 
