d66265cfcd0c840ebf2991d9381d96dbc69be184
KippaZou <donald86210@yahoo.com>
2021-02-15 12:01:44 +0800

    max consecutive ones

---
+++ golang/485.findMaxConsecutiveOnes/findMaxConsecutiveOnes.go
@@ -0,0 +1,21 @@
+package _85_findMaxConsecutiveOnes
+
+func findMaxConsecutiveOnes(nums []int) int {
+	x, y := 0, 0
+	for _, num := range nums {
+		if num != 0 {
+			y++
+		} else {
+			x = max(x, y)
+			y = 0
+		}
+	}
+	return max(x, y)
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
