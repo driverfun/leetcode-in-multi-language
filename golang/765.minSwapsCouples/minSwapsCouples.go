0310397a013fc1a7627992dfadbd0b4354b098ba
KippaZou <donald86210@yahoo.com>
2021-02-15 12:01:24 +0800

    min swap couples v3

--- golang/765.minSwapsCouples/minSwapsCouples.go
+++ golang/765.minSwapsCouples/minSwapsCouples.go
@@ -45,3 +45,86 @@ func MinSwapsCouplesV2(row []int) int {
 	}
 	return count
 }
+
+// 广度优先搜索
+func MinSwapsCouplesV3(row []int) int {
+	n := len(row)
+	graph := make([][]int, n/2)
+	for i := 0; i < n; i += 2 {
+		l, r := row[i]/2, row[i+1]/2
+		if l != r {
+			graph[l] = append(graph[l], r)
+			graph[r] = append(graph[r], l)
+		}
+	}
+	res := 0
+	vis := make([]bool, n/2)
+	for i, vs := range vis {
+		if !vs {
+			vis[i] = true
+			count := 0
+			q := []int{i}
+			for len(q) > 0 {
+				count++
+				v := q[0]
+				q = q[1:]
+				for _, w := range graph[v] {
+					if !vis[w] {
+						vis[w] = true
+						q = append(q, w)
+					}
+				}
+			}
+			res += count - 1
+		}
+	}
+	return res
+}
+
+// 并查集
+type unionFind struct {
+	parent, size []int
+	setCount     int // 当前连通分量数目
+}
+
+func newUnionFind(n int) *unionFind {
+	parent := make([]int, n)
+	size := make([]int, n)
+	for i := range parent {
+		// 每个 parent 都是自己，每个 set 的 size 都是 1，初始 set 个数 n
+		parent[i] = i
+		size[i] = 1
+	}
+	return &unionFind{parent: parent, size: size, setCount: n}
+}
+
+func (uf *unionFind) find(x int) int {
+	if uf.parent[x] != x {
+		// 1. path compression, 所有父都指向根节点
+		uf.parent[x] = uf.find(uf.parent[x])
+	}
+	return uf.parent[x]
+}
+
+func (uf *unionFind) union(x, y int) {
+	fx, fy := uf.find(x), uf.find(y)
+	if fx == fy {
+		return
+	}
+	// 2. union by size 把 size 小的树 union 到 rank 比较大的树
+	if uf.size[x] < uf.size[y] {
+		fx, fy = fy, fx
+	}
+	uf.size[fx] += uf.size[fy]
+	uf.parent[fy] = fx
+	uf.setCount--
+}
+
+func MinSwapsCouplesV4(row []int) int {
+	n := len(row)
+	uf := newUnionFind(n / 2)
+	for i := 0; i < n; i += 2 {
+		uf.union(row[i]/2, row[i+1]/2)
+	}
+	return n/2 - uf.setCount
+}
