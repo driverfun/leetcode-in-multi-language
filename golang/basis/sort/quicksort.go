7347fc5975d103cbc67bcc7847333ad82e3429de
KippaZou <donald86210@yahoo.com>
2021-02-16 11:28:59 +0800

    quick sort

--- golang/basis/sort/quicksort.go
+++ golang/basis/sort/quicksort.go
@@ -1,1 +1,26 @@
 package sort
+
+func QuickSort(nums []int) {
+	if len(nums) < 2 {
+		return
+	}
+	pivot := nums[len(nums)-1]
+	left, right := 0, len(nums)-2
+	for left != right {
+		if nums[left] < pivot {
+			left++
+		} else if nums[right] > pivot {
+			right--
+		} else {
+			nums[left], nums[right] = nums[right], nums[left]
+		}
+	}
+	if nums[left] > pivot {
+		nums[left], nums[len(nums)-1] = nums[len(nums)-1], nums[left]
+		QuickSort(nums[:left])
+		QuickSort(nums[right+1:])
+	} else {
+		QuickSort(nums[:left+1])
+		QuickSort(nums[right+1:])
+	}
+}
---
+++ golang/basis/sort/sort_test.go
@@ -0,0 +1,22 @@
+package sort
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func randomInts(length int) []int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Perm(length)
+}
+
+func TestQuickSort(t *testing.T) {
+	nums := randomInts(1000)
+	QuickSort(nums)
+	for i := 0; i < len(nums)-1; i++ {
+		require.Greater(t, nums[i+1], nums[i])
+	}
+}
