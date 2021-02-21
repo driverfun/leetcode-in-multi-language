cd7544b92ca9e808fc49ced467704a99306db1cd
KippaZou <donald86210@yahoo.com>
2021-02-21 15:51:34 +0800

    105. buildTree

---
+++ golang/tree/105.buildTree.go
@@ -0,0 +1,17 @@
+package _tree
+
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 {
+		return nil
+	}
+	root := &TreeNode{preorder[0], nil, nil}
+	i := 0
+	for ; i < len(inorder); i++ {
+		if inorder[i] == preorder[0] {
+			break
+		}
+	}
+	root.Left = buildTree(preorder[1:i+1], inorder[:i])
+	root.Right = buildTree(preorder[i+1:], inorder[i+1:])
+	return root
+}
