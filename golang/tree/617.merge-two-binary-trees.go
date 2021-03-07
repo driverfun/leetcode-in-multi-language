1c0cc3e3e52aaf29ccce1a7c98a429e25be2f0cf
KippaZou <donald86210@yahoo.com>
2021-03-07 09:58:19 +0800

    617.merge-two-binary-trees

---
+++ golang/tree/617.merge-two-binary-trees.go
@@ -0,0 +1,14 @@
+package _tree
+
+/*
+简单的递归即可
+*/
+func mergeTrees(root1 *TreeNode, root2 *TreeNode) *TreeNode {
+	if root1 == nil {
+		return root2
+	}
+	if root2 == nil {
+		return root1
+	}
+	return &TreeNode{Val: root1.Val + root2.Val, Left: mergeTrees(root1.Left, root2.Left), Right: mergeTrees(root1.Right, root2.Right)}
+}
